dfdbe7d8efc36f0d0ed661406d772fd9c043b41f
Santianiosss <61583128+Santianiosss@users.noreply.github.com>
2022-09-25 16:09:04 +0300

    1

---
+++ json/myHome/plans.js
@@ -0,0 +1,37 @@
+Object.assign(window.gameData, {MyHomePlans: [
+    {
+      title: 'Вариант №1',
+      pathImg: 'tablet-logistic-company/warehouses/monitor_1.jpg',
+      cost: '70000',
+      active: true,
+
+    },
+    {
+      title: 'Вариант №2',
+      pathImg: 'tablet-logistic-company/warehouses/monitor_1.jpg',
+      cost: '80000',
+      active: false,
+
+    },
+    {
+      title: 'Вариант №3',
+      pathImg: 'tablet-logistic-company/warehouses/monitor_2.jpg',
+      cost: '90000',
+      active: false,
+
+    },
+    {
+      title: 'Вариант №4',
+      pathImg: 'tablet-logistic-company/warehouses/monitor_3.jpg',
+      cost: '100000',
+      active: false,
+
+    },
+    {
+      title: 'Вариант №5',
+      pathImg: 'tablet-logistic-company/warehouses/monitor_4.jpg',
+      cost: '110000',
+      active: false,
+
+    },
+  ]});
